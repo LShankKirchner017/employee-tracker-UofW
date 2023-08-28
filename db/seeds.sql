aad0da6a379c76b91255c3b2800692732125c6ba
Lindsay <lshank017@gmail.com>
2023-08-28 15:18:07 -0500

    started seeds, need to finish questions.js

---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO departments (name) VALUES
+("Upper Management"),
+("Management"),
+("Sales"), 
+("Accounting"),
+("Human Resources"),
+("Clerical")
+
+INSERT INTO roles (title, salary, department_id) VALUES()
+("Regional Manager"),
+("Assistant Regional Manager"),
+("Assistant to the Regional Manager"),
+("Sales"), 
+("Accounting"),
+("Secretary"),
+("Party Planning Committee")
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
