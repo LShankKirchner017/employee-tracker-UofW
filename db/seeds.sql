0282578e51f6b2cf88face849d979efdcaa2eafb
Lindsay <lshank017@gmail.com>
2023-08-31 17:57:59 -0500

    fixed schema and seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,18 @@
-INSERT INTO departments (name) VALUES
+INSERT INTO departments (department_name) VALUES
 ("Upper Management"),
 ("Management"),
 ("Sales"), 
 ("Accounting"),
 ("Human Resources"),
-("Clerical")
+("Clerical");
 
-INSERT INTO roles (title, salary, department_id) VALUES()
+INSERT INTO roles (title, salary, department_id) VALUES
 ("Regional Manager", 175000, 1),
 ("Assistant Regional Manager", 100000, 2),
 ("Assistant to the Regional Manager", 99999, 3),
 ("Sales", 850000, 4), 
 ("Accounting", 50000, 5),
-("Secretary" 45000, 6)
+("Secretary", 45000, 6);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
 ("Michael", "Scott", 1, null),
@@ -20,7 +20,7 @@ INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
 ("Jim", "Halpert", 2, 1),
 ("Andrew", "Bernard", 3, 2),
 ("Stanley", "Hudson", 4, 1),
-("Phyllis", "Lapin-Vance", 4, 1)
+("Phyllis", "Lapin-Vance", 4, 1),
 ("Kevin", "Malone", 5, 1),
 ("Angela", "Martin", 5, 1),
-("Pam", "Beasley", 6, 2)
+("Pam", "Beasley", 6, 2);
