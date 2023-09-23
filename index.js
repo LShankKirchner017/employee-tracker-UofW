71fbf3736d48818587ac290594ce77def2e9bb68
Lindsay <lshank017@gmail.com>
2023-09-23 18:09:19 -0500

    added index.js and wrote switch functions

---
+++ index.js
@@ -0,0 +1,58 @@
+const inquirer = require('inquirer')
+const {
+    viewAllDepartments,
+    addDepartment,
+    viewAllEmployees,
+    addEmployee,
+    updateEmployee,
+    viewAllRoles,
+    addRole,
+    existMenu
+
+} = require('./utils/userSelectionFunctions')
+
+inquirer.prompt({
+    type: 'list',
+    name: 'userSelection',
+    message: 'What would you like to view?',
+    choices: ['View all departments', 
+    'View all roles',
+    'View all employees',
+    'Add a department',
+    'Add a role',
+    'Add an employee',
+    'Update an employee/role',
+    'Exit the application'],
+    default: 'View all departments'
+})
+.then((answers) => {
+    switch(answers.userSelection)
+    {
+     case "View all departments":
+        viewAllDepartments()
+        break;
+    case "View all roles":
+         viewAllRoles()
+        break;
+    case "View all employees":
+        viewAllEmployees()
+        break;
+    case "Add a department":
+        addDepartment()
+        break;
+    case "Add a role":
+        addRole()
+        break;
+    case "Add an employee":
+        addEmployee()
+        break;
+    case "Update an employee/role":
+        updateEmployee()
+        break;
+    case "Exit the application":
+        console.log('Thank you! Goodbye!')
+        existMenu()
+    }
+}) .catch((err) => {
+    console.error(err)
+})
